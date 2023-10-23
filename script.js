073664f1023ab71ef58f5315e04c7727fb36df55
Elvis <steevenbailon23@gmail.com>
2023-10-23 12:30:29 -0500

    Se realizo el proceso de la suma

---
+++ script.js
@@ -0,0 +1,9 @@
+var num1 = document.querySelector("#num1");
+var num2 = document.querySelector("#num2");
+
+var resultado = document.querySelector("#Resultado");
+
+function suma(){
+    let suma = parseInt(num1.value)+parseInt(num2.value);
+    resultado.innerHTML=" El Resultado es: " +(suma);
+}
